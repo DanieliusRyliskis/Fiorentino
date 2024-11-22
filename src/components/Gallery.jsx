182bf5cdaf2e97a345e89fb6f7f801560f5a802d
DanieliusRyliskis <ryliskisdanielius@gmail.com>
2024-11-22 11:38:43 +0200

    Generate images in the react component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,67 +2,105 @@ import { useState } from 'react';
 
 function Gallery(props) {
     // Here Goes Variables And Functions
-    console.log(props.optimizedImages);
+    // console.log(props.optimizedImages.avif.left.length);
+
+    const leftPictures = []
+    const middlePictures = []
+    const rightPictures = []
+    
+    for (let i = 0; i < props.optimizedImages.jpg.left.length; i++){
+        leftPictures.push(
+            <picture key={i}>
+                <source 
+                srcSet={props.optimizedImages.avif.left[i].srcSet.attribute}
+                sizes='(max-width: 40em) 600px, (min-width: 40.063em) and (max-width: 80em) 900px, 600px'
+                type='image/avif'
+                />  
+                <source 
+                srcSet={props.optimizedImages.webp.left[i].srcSet.attribute}
+                sizes='(max-width: 40em) 600px, (min-width: 40.063em) and (max-width: 80em) 900px, 600px'
+                type='image/webp'
+                />  
+                <img 
+                src={props.optimizedImages.jpg.left[i].src} 
+                srcSet={props.optimizedImages.jpg.left[i].srcSet.attribute}
+                sizes='(max-width: 40em) 600px, (min-width: 40.063em) and (max-width: 80em) 900px, 600px'
+                className="w-full lg:h-[21.563rem] lg:object-cover cursor-pointer"
+                decoding='async'
+                alt="" 
+                />
+            </picture>
+        );
+    }
+
+    for (let i = 0; i < props.optimizedImages.jpg.middle.length; i++){
+        middlePictures.push(
+            <picture key={i}>
+                <source 
+                srcSet={props.optimizedImages.avif.middle[i].srcSet.attribute}
+                sizes='(max-width: 40em) 600px, (min-width: 40.063em) and (max-width: 80em) 900px, 600px'
+                type='image/avif'
+                />  
+                <source 
+                srcSet={props.optimizedImages.webp.middle[i].srcSet.attribute}
+                sizes='(max-width: 40em) 600px, (min-width: 40.063em) and (max-width: 80em) 900px, 600px'
+                type='image/webp'
+                />  
+                <img 
+                src={props.optimizedImages.jpg.middle[i].src} 
+                srcSet={props.optimizedImages.jpg.middle[i].srcSet.attribute}
+                sizes='(max-width: 40em) 600px, (min-width: 40.063em) and (max-width: 80em) 900px, 600px'
+                className="w-full lg:h-[43.875rem] lg:object-cover cursor-pointer"
+                decoding='async'
+                alt="" 
+                />
+            </picture>
+        );
+    }
+    for (let i = 0; i < props.optimizedImages.jpg.right.length; i++){
+        rightPictures.push(
+            <picture key={i}>
+                <source 
+                srcSet={props.optimizedImages.avif.right[i].srcSet.attribute}
+                sizes='(max-width: 40em) 600px, (min-width: 40.063em) and (max-width: 80em) 900px, 600px'
+                type='image/avif'
+                />  
+                <source 
+                srcSet={props.optimizedImages.webp.right[i].srcSet.attribute}
+                sizes='(max-width: 40em) 600px, (min-width: 40.063em) and (max-width: 80em) 900px, 600px'
+                type='image/webp'
+                />  
+                <img 
+                src={props.optimizedImages.jpg.right[i].src} 
+                srcSet={props.optimizedImages.jpg.right[i].srcSet.attribute}
+                sizes='(max-width: 40em) 600px, (min-width: 40.063em) and (max-width: 80em) 900px, 600px'
+                className="w-full lg:h-[21.563rem] lg:object-cover cursor-pointer"
+                decoding='async'
+                alt="" 
+                />
+            </picture>
+        );
+    }
+
 
     // In Return Goes The HTML And JSX
     // You Can Only Return Single Element
     return (
         <div
-            class="flex flex-col lg:flex-row gap-4 w-fit lg:w-[95%] mx-auto mb-[clamp(4rem,_3.2rem_+_4vw,_8rem)] xl:max-w-[83.5rem]"
+            className="flex flex-col lg:flex-row gap-4 w-fit lg:w-[95%] mx-auto mb-[clamp(4rem,_3.2rem_+_4vw,_8rem)] xl:max-w-[83.5rem]"
             id="imageContainer"
         >
-            <div class="flex flex-col w-[90%] mx-auto gap-4 xl:max-w-[28.75rem]">
-                {left.map((c) => (
-                    <Picture
-                        src={images[c.file_path].default}
-                        widths={[600, 900]}
-                        sizes={
-                            '(max-width: 40em) 600px, (min-width: 40.063em) and (max-width: 64em) 900px, (min-width: 64.063em) 600px'
-                        }
-                        class={`w-full lg:h-[21.563rem] lg:object-cover cursor-pointer`}
-                        alt={c.title}
-                        formats={['avif', 'webp']}
-                        fallbackFormat="jpg"
-                        quality={'high'}
-                    />
-                ))}
+            <div className="flex flex-col w-[90%] mx-auto gap-4 xl:max-w-[28.75rem]">
+                {leftPictures}
             </div>
             <div class="flex flex-col w-[90%] mx-auto gap-4 xl:max-w-[23.75rem]">
-                {middle.map((c) => (
-                    <Picture
-                        src={images[c.file_path].default}
-                        widths={[600, 900]}
-                        sizes={
-                            '(max-width: 40em) 600px, (min-width: 40.063em) and (max-width: 64em) 900px, (min-width: 64.063em) 600px'
-                        }
-                        class="w-full lg:h-[43.875rem] lg:object-cover cursor-pointer aspec"
-                        alt={c.title}
-                        formats={['avif', 'webp']}
-                        fallbackFormat="jpg"
-                        quality={'high'}
-                    />
-                ))}
+                {middlePictures}
             </div>
-
             <div class="flex flex-col w-[90%] mx-auto gap-4 xl:max-w-[28.75rem]">
-                {right.map((c) => (
-                    <Picture
-                        src={images[c.file_path].default}
-                        widths={[600, 900]}
-                        sizes={
-                            '(max-width: 40em) 600px, (min-width: 40.063em) and (max-width: 64em) 900px, (min-width: 64.063em) 600px'
-                        }
-                        class={`w-full lg:h-[21.563rem] lg:object-cover cursor-pointer`}
-                        alt={c.title}
-                        alt={c.title}
-                        formats={['avif', 'webp']}
-                        fallbackFormat="jpg"
-                        quality={'high'}
-                    />
-                ))}
+                {rightPictures}
             </div>
         </div>
     );
 }
-
 export default Gallery;
+
---
+++ src/components/obj.jsx
@@ -0,0 +1,114 @@
+// {
+//     "rawOptions": {
+//         "src": {
+//             "src": "/@fs/home/daniel/repos/Fiorentino/src/assets/pictures/gallery_1_preview.jpg?origWidth=1844&origHeight=1380&origFormat=jpg",
+//             "width": 1844,
+//             "height": 1380,
+//             "format": "jpg"
+//         },
+//         "format": "avif",
+//         "widths": [
+//             600,
+//             900,
+//             1844
+//         ],
+//         "quality": "high"
+//     },
+//     "options": {
+//         "src": {
+//             "src": "/@fs/home/daniel/repos/Fiorentino/src/assets/pictures/gallery_1_preview.jpg?origWidth=1844&origHeight=1380&origFormat=jpg",
+//             "width": 1844,
+//             "height": 1380,
+//             "format": "jpg"
+//         },
+//         "format": "avif",
+//         "widths": [
+//             600,
+//             900,
+//             1844
+//         ],
+//         "quality": "high"
+//     },
+//     "src": "/_image?href=%2F%40fs%2Fhome%2Fdaniel%2Frepos%2FFiorentino%2Fsrc%2Fassets%2Fpictures%2Fgallery_1_preview.jpg%3ForigWidth%3D1844%26origHeight%3D1380%26origFormat%3Djpg&q=high&f=avif",
+//     "srcSet": {
+//         "values": [
+//             {
+//                 "transform": {
+//                     "src": {
+//                         "src": "/@fs/home/daniel/repos/Fiorentino/src/assets/pictures/gallery_1_preview.jpg?origWidth=1844&origHeight=1380&origFormat=jpg",
+//                         "width": 1844,
+//                         "height": 1380,
+//                         "format": "jpg"
+//                     },
+//                     "format": "avif",
+//                     "widths": [
+//                         600,
+//                         900,
+//                         1844
+//                     ],
+//                     "quality": "high",
+//                     "width": 600
+//                 },
+//                 "url": "/_image?href=%2F%40fs%2Fhome%2Fdaniel%2Frepos%2FFiorentino%2Fsrc%2Fassets%2Fpictures%2Fgallery_1_preview.jpg%3ForigWidth%3D1844%26origHeight%3D1380%26origFormat%3Djpg&w=600&q=high&f=avif",
+//                 "descriptor": "600w",
+//                 "attributes": {
+//                     "type": "image/avif"
+//                 }
+//             },
+//             {
+//                 "transform": {
+//                     "src": {
+//                         "src": "/@fs/home/daniel/repos/Fiorentino/src/assets/pictures/gallery_1_preview.jpg?origWidth=1844&origHeight=1380&origFormat=jpg",
+//                         "width": 1844,
+//                         "height": 1380,
+//                         "format": "jpg"
+//                     },
+//                     "format": "avif",
+//                     "widths": [
+//                         600,
+//                         900,
+//                         1844
+//                     ],
+//                     "quality": "high",
+//                     "width": 900
+//                 },
+//                 "url": "/_image?href=%2F%40fs%2Fhome%2Fdaniel%2Frepos%2FFiorentino%2Fsrc%2Fassets%2Fpictures%2Fgallery_1_preview.jpg%3ForigWidth%3D1844%26origHeight%3D1380%26origFormat%3Djpg&w=900&q=high&f=avif",
+//                 "descriptor": "900w",
+//                 "attributes": {
+//                     "type": "image/avif"
+//                 }
+//             },
+//             {
+//                 "transform": {
+//                     "src": {
+//                         "src": "/@fs/home/daniel/repos/Fiorentino/src/assets/pictures/gallery_1_preview.jpg?origWidth=1844&origHeight=1380&origFormat=jpg",
+//                         "width": 1844,
+//                         "height": 1380,
+//                         "format": "jpg"
+//                     },
+//                     "format": "avif",
+//                     "widths": [
+//                         600,
+//                         900,
+//                         1844
+//                     ],
+//                     "quality": "high"
+//                 },
+//                 "url": "/_image?href=%2F%40fs%2Fhome%2Fdaniel%2Frepos%2FFiorentino%2Fsrc%2Fassets%2Fpictures%2Fgallery_1_preview.jpg%3ForigWidth%3D1844%26origHeight%3D1380%26origFormat%3Djpg&q=high&f=avif",
+//                 "descriptor": "1844w",
+//                 "attributes": {
+//                     "type": "image/avif"
+//                 }
+//             }
+//         ],
+//         "attribute": "/_image?href=%2F%40fs%2Fhome%2Fdaniel%2Frepos%2FFiorentino%2Fsrc%2Fassets%2Fpictures%2Fgallery_1_preview.jpg%3ForigWidth%3D1844%26origHeight%3D1380%26origFormat%3Djpg&w=600&q=high&f=avif 600w, /_image?href=%2F%40fs%2Fhome%2Fdaniel%2Frepos%2FFiorentino%2Fsrc%2Fassets%2Fpictures%2Fgallery_1_preview.jpg%3ForigWidth%3D1844%26origHeight%3D1380%26origFormat%3Djpg&w=900&q=high&f=avif 900w, /_image?href=%2F%40fs%2Fhome%2Fdaniel%2Frepos%2FFiorentino%2Fsrc%2Fassets%2Fpictures%2Fgallery_1_preview.jpg%3ForigWidth%3D1844%26origHeight%3D1380%26origFormat%3Djpg&q=high&f=avif 1844w"
+//     },
+//     "attributes": {
+//         "width": 1844,
+//         "height": 1380,
+//         "loading": "lazy",
+//         "decoding": "async"
+//     },
+//     "category": "Left (Horizontal)",
+//     "publish_date": "2024-11-19T18:19:00.000Z"
+// }
